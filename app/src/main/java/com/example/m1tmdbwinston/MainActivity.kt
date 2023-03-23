4dd049c7810e0a0f8326cf92cd937a9b9bd6921f
Ninwost <pelletier.winston@gmail.com>
2023-03-23 11:39:49 +0100

    marche

--- app/src/main/java/com/example/m1tmdbwinston/MainActivity.kt
+++ app/src/main/java/com/example/m1tmdbwinston/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.example.m1tmdbwinston
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
 import com.example.m1tmdbwinston.databinding.ActivityMainBinding
 import com.example.m1tmdbwinston.model.Person
 import com.example.m1tmdbwinston.model.PersonPopularResponse
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     val LOGTAG = MainActivity::class.simpleName
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var personPopularAdapter: PersonPopularAdapter
     private var persons = arrayListOf<Person>()
+    private var totalResults = 0
+    private var totalPages = Int.MAX_VALUE
+    private var curPage = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +35,21 @@ class MainActivity : AppCompatActivity() {
         // Init recycler view
         binding.popularPersonRv.setHasFixedSize(true)
         binding.popularPersonRv.layoutManager = LinearLayoutManager(this)
-        personPopularAdapter = PersonPopularAdapter(persons)
+        personPopularAdapter = PersonPopularAdapter(persons, this)
         binding.popularPersonRv.adapter = personPopularAdapter
+        binding.popularPersonRv.addOnScrollListener(object : androidx.recyclerview.widget.RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == SCROLL_STATE_IDLE && !recyclerView.canScrollVertically(1)) {
+                    if (curPage < totalPages) {
+                        curPage++
+                        loadPage(curPage)
+                    }
+                }
+            }
+        })
 
-        loadPage(1)
+        loadPage(curPage)
 
     }
 
@@ -38,22 +57,29 @@ class MainActivity : AppCompatActivity() {
         val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
 
         val call = tmdbapi.getPopularPerson(TMDB_API_KEY, page)
-
+        binding.progressWheel.visibility = VISIBLE
         call.enqueue(object : Callback<PersonPopularResponse> {
             override fun onResponse(
                 call: Call<PersonPopularResponse>,
                 response: Response<PersonPopularResponse>
             ) {
-                if (response.isSuccessful) {
+                if (response.isSuccessful && response.body() != null) {
+                    Toast.makeText(applicationContext, "Page $page loaded", Toast.LENGTH_SHORT).show()
                     persons.addAll(response.body()?.results!!)
+                    totalResults = response.body()!!.totalResults!!
+                    totalPages = response.body()!!.totalPages!!
                     personPopularAdapter.notifyDataSetChanged()
+                    personPopularAdapter.setMaxPopularity()
+                    binding.totalResultsTv.text = getString(R.string.total_results_text,persons.size, totalResults)
                 } else {
                     Log.e(LOGTAG, "Call to getPopularPerson failed with error ${response.code()}")
                 }
+                binding.progressWheel.visibility = GONE
             }
 
             override fun onFailure(call: Call<PersonPopularResponse>, t: Throwable) {
                 Log.e(LOGTAG,"Call to getPopularPerson failed")
+                binding.progressWheel.visibility = GONE
             }
         })
     }
--- app/src/main/java/com/example/m1tmdbwinston/PersonPopularAdapter.kt
+++ app/src/main/java/com/example/m1tmdbwinston/PersonPopularAdapter.kt
@@ -1,54 +1,92 @@
 package com.example.m1tmdbwinston
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.m1tmdbwinston.ApiClient.Companion.IMAGE_BASE_URL
+import com.squareup.picasso.Picasso
+import android.content.Context
+import android.graphics.Color
+import com.example.m1tmdbwinston.databinding.PersonItemBinding
 import com.example.m1tmdbwinston.ApiClient.Companion.IMAGE_BASE_URL
 import com.example.m1tmdbwinston.model.Person
 
-import android.view.LayoutInflater
-import android.view.PixelCopy
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
+class PersonPopularAdapter(private val persons: ArrayList<Person>, context: Context) : RecyclerView.Adapter<PersonPopularAdapter.PersonItemViewHolder>(){
 
-class PersonPopularAdapter(val persons: ArrayList<Person>) : RecyclerView.Adapter<PersonPopularAdapter.ViewHolder>(){
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    class PersonItemViewHolder(var binding: PersonItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private var maxPopularity:Double = 0.0
+    private val scoreRatings: Array<String> = context.resources.getStringArray(R.array.score_rating)
+    private val ratingColors: Array<String> = context.resources.getStringArray(R.array.rating_colors)
 
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    init {
+        setMaxPopularity()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonItemViewHolder {
+        val binding = PersonItemBinding.inflate((LayoutInflater.from(parent.context)), parent, false)
+        return PersonItemViewHolder(binding)
+    }
+
+    override fun getItemCount() = persons.size
+
+    override fun onBindViewHolder(holder: PersonItemViewHolder, position: Int) {
+        val curItem = persons.get(position)
+        holder.binding.nameTv.text = curItem.name
+        holder.binding.knownForTv.text = curItem.knownForDepartment
+        holder.binding.popularityTv.text = curItem.popularity.toString()
+        Picasso.get()
+            .load(ApiClient.IMAGE_BASE_URL + curItem.profilePath)
+            .placeholder(android.R.drawable.progress_horizontal)
+            .error(android.R.drawable.stat_notify_error)
+            .into(holder.binding.photoIv)
+
+        holder.binding.scoreGaugeView.updateScore(
+            getRating(curItem.popularity,maxPopularity),
+            getScoreColor(curItem.popularity,maxPopularity),
+            curItem.popularity!!.toFloat(),
+            maxPopularity.toInt()
+        )
+    }
+
+    fun setMaxPopularity() {
+        maxPopularity = 0.0
+        for (p in persons) {
+            if (p.popularity != null && p.popularity!! > maxPopularity) maxPopularity = p.popularity!!
+        }
+    }
+
+    private fun getRating(value:Double?, max:Double):String {
+        val i:Int = if (value != null && max > 0.0) ((scoreRatings.size -1) * value/max).toInt() else 0
+        return scoreRatings[i]
+    }
+
+    private fun getScoreColor(value:Double?, max:Double):Int {
+        val i:Int = if (value != null && max > 0.0) ((ratingColors.size -1) * value/max).toInt() else 0
+        return try {
+            ratingColors[i].toInt()
+        } catch (e : Exception) {
+            Color.RED
+        }
+    }
+
+    /* class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameTv: TextView
         val knownForTv: TextView
         val popularityTv: TextView
         val photoIv: ImageView
+        val scoreGaugeView: ScoreGaugeView
 
         init {
             nameTv = view.findViewById(R.id.name_tv)
             knownForTv = view.findViewById(R.id.known_for_tv)
             popularityTv = view.findViewById(R.id.popularity_tv)
             photoIv = view.findViewById(R.id.photo_iv)
+            scoreGaugeView = view.findViewById(R.id.score_gauge_view)
         }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount() = persons.size
-
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val curItem = persons.get(position)
-        holder.nameTv.text = curItem.name
-        holder.knownForTv.text = curItem.knownForDepartment
-        holder.popularityTv.text = curItem.popularity.toString()
-        Picasso.get()
-            .load(IMAGE_BASE_URL + curItem.profilePath)
-            .placeholder(android.R.drawable.progress_horizontal)
-            .error(android.R.drawable.stat_notify_error)
-            .into(holder.photoIv)
-
-
-    }
-
+    }*/
 }
+
